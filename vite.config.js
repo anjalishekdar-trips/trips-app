d0ed553ad268cfe81629188ef6a1a54cb6439a0e
anjalishekdar-trips <anjali.shekdar@gmail.com>
2026-06-02 20:44:39 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -19,22 +19,17 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
-          }
-        ]
+    src: 'icon-512.jpg',
+    sizes: '512x512',
+    type: 'image/jpeg'
+  },
+  {
+    src: 'icon-512.jpg',
+    sizes: '512x512',
+    type: 'image/jpeg',
+    purpose: 'any maskable'
+  }
+]
       }
     })
   ]
